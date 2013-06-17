c4d1e6e7bbc6f8722fc1da1654b075cd9f760c39
Denis Ilguzin <dilguzin@tocobox.com>
2013-06-17 10:18:51 +0400

    Added DelimiterBasedFrameDecoder handler after ssl decode handler for frame limits decoding

--- sitebricks-mail/src/main/java/com/google/sitebricks/mail/UBoundMailClientPipelineFactory.java
+++ sitebricks-mail/src/main/java/com/google/sitebricks/mail/UBoundMailClientPipelineFactory.java
@@ -3,6 +3,8 @@ package com.google.sitebricks.mail;
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
 import org.jboss.netty.channel.Channels;
+import org.jboss.netty.handler.codec.frame.DelimiterBasedFrameDecoder;
+import org.jboss.netty.handler.codec.frame.Delimiters;
 import org.jboss.netty.handler.codec.string.StringDecoder;
 import org.jboss.netty.handler.codec.string.StringEncoder;
 import org.jboss.netty.handler.ssl.SslHandler;
@@ -27,6 +29,8 @@ class UBoundMailClientPipelineFactory implements ChannelPipelineFactory {
     SslHandler sslHandler = new SslHandler(sslEngine);
     sslHandler.setEnableRenegotiation(true);
     pipeline.addLast("ssl", sslHandler);
+
+    pipeline.addLast("framer", new DelimiterBasedFrameDecoder(1024, Delimiters.lineDelimiter()));
 
     pipeline.addLast("decoder", new StringDecoder());
     pipeline.addLast("encoder", new StringEncoder());
